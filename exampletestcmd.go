6165072f904454d7d56d0e802f40121091924483
nas <nas.silver.bullet@gmail.com>
2019-11-24 00:11:58 +0900

    :recycle: Clean directory

--- exampletestcmd.go
+++ exampletestcmd.go
@@ -1,7 +1,11 @@
 package atc
 
 import (
-	"github.com/NasSilverBullet/atc/internal/task"
+	"fmt"
+	"io/ioutil"
+	"os"
+
+	"github.com/NasSilverBullet/atc/internal"
 	"github.com/urfave/cli"
 )
 
@@ -11,11 +15,25 @@ func GetExampleTestCmd() cli.Command {
 		Aliases: []string{"t"},
 		Usage:   "test io",
 		Action: func(c *cli.Context) error {
-			e, err := task.GetExamples("abc144", "a")
+			dir, err := os.Getwd()
 			if err != nil {
 				return err
 			}
+			fs, err := ioutil.ReadDir(dir)
+
+			if err != nil {
+				return err
+			}
+			for _, f := range fs {
+				fmt.Println(f.Name())
+			}
+			e, err := internal.GetExamples("abc144", "a")
+			if err != nil {
+				return err
+			}
+
 			_ = e
+
 			return nil
 		},
 	}
--- internal/example_test.go
+++ internal/example_test.go
@@ -1,11 +1,11 @@
-package task_test
+package internal_test
 
 import (
 	"os"
 	"reflect"
 	"testing"
 
-	"github.com/NasSilverBullet/atc/internal/task"
+	"github.com/NasSilverBullet/atc/internal"
 )
 
 const testFile = "tmp.py"
@@ -18,20 +18,20 @@ func TestGetExamples(t *testing.T) {
 	tests := []struct {
 		name string
 		args args
-		want task.Examples
+		want internal.Examples
 	}{
 		// TODO できるようにする
-		// {"Success", args{"abc001", "a"}, &task.Example{"15\n10", "5", "0\n0", "0", "5 20", "-15"}, false},
+		// {"Success", args{"abc001", "a"}, &internal.Example{"15\n10", "5", "0\n0", "0", "5 20", "-15"}, false},
 		// TODO できるようにする
-		// {"Success", args{"abc020", "a"}, &task.Example{"1", "ABC", "2", "chokudai", "", ""}, false},
-		{"ABC042", args{"abc042", "a"}, task.Examples{&task.Example{"5 5 7\n", "YES\n"}, &task.Example{"7 7 5\n", "NO\n"}}},
-		{"ABC145", args{"abc145", "f"}, task.Examples{&task.Example{"4 1\n2 3 4 1\n", "3\n"}, &task.Example{"6 2\n8 6 9 1 2 1\n", "7\n"}, &task.Example{"10 0\n1 1000000000 1 1000000000 1 1000000000 1 1000000000 1 1000000000\n", "4999999996\n"}}},
-		{"ARC072", args{"arc072", "b"}, task.Examples{&task.Example{"2 1\n", "Brown\n"}, &task.Example{"5 0\n", "Alice\n"}, &task.Example{"0 0\n", "Brown\n"}, &task.Example{"4 8\n", "Alice\n"}}},
+		// {"Success", args{"abc020", "a"}, &internal.Example{"1", "ABC", "2", "chokudai", "", ""}, false},
+		{"ABC042", args{"abc042", "a"}, internal.Examples{&internal.Example{"5 5 7\n", "YES\n"}, &internal.Example{"7 7 5\n", "NO\n"}}},
+		{"ABC145", args{"abc145", "f"}, internal.Examples{&internal.Example{"4 1\n2 3 4 1\n", "3\n"}, &internal.Example{"6 2\n8 6 9 1 2 1\n", "7\n"}, &internal.Example{"10 0\n1 1000000000 1 1000000000 1 1000000000 1 1000000000 1 1000000000\n", "4999999996\n"}}},
+		{"ARC072", args{"arc072", "b"}, internal.Examples{&internal.Example{"2 1\n", "Brown\n"}, &internal.Example{"5 0\n", "Alice\n"}, &internal.Example{"0 0\n", "Brown\n"}, &internal.Example{"4 8\n", "Alice\n"}}},
 	}
 
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			got, err := task.GetExamples(tt.args.contest, tt.args.question)
+			got, err := internal.GetExamples(tt.args.contest, tt.args.question)
 			if err != nil {
 				t.Errorf("Unexpected error: %v", err)
 				return
@@ -92,7 +92,7 @@ func TestExample_Run(t *testing.T) {
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			e := &task.Example{
+			e := &internal.Example{
 				Input:  tt.fields.Input,
 				Output: tt.fields.Output,
 			}
