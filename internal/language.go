1d4aa418ae75cd9732fa78cd9453800faa36f5eb
nas <nas.silver.bullet@gmail.com>
2019-11-24 11:58:22 +0900

    :+1: Update GetLanguagePaht

--- internal/language.go
+++ internal/language.go
@@ -5,14 +5,12 @@ import (
 )
 
 // GetLanguagePath return local laguange path
-// TODO yamlの設定ファイルから読めるようにする
 // TODO 実行にコンパイルの必要な言語にも対応する
-func GetLanguagePath(fileName string) string {
+func (lc LanguagesConfig) GetLanguagePath(fileName string) string {
 	ext := filepath.Ext(fileName)
-	switch ext {
-	case ".py":
-		return "~/.pyenv/versions/pypy3.6-7.2.0/bin/pypy3"
-	default:
+	if ext == "" {
 		return ""
 	}
+
+	return lc[ext[1:]]
 }
